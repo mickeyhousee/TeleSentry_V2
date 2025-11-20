18c7080087936a4cd29dfce31ee94f4176197058
João Grilo <joaogrilo2222@gmail.com>
2025-11-20 20:26:30 +0000

    Copy older webserver to the new repo

---
+++ web/telegram_webserver/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Telegram Webserver</title>
+    <link rel="stylesheet" href="/telegram_webserver/css/styles.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+</head>
+<body>
+    <div class="sidebar">
+        <div class="menu-icon">
+            <img src="/telegram_webserver/images/menu.png" alt="Menu">
+        </div>
+        <div class="logo" onclick="loadRoom('roompred')">
+            <img src="/telegram_webserver/images/logo_cociber.png" alt="Logo">
+        </div>
+        <a href="#" onclick="loadRoom('room3')">
+            <span class="text">PESQUISA</span>
+        </a>
+        <a href="#" onclick="loadRoom('room2')">
+            <span class="text">BOT´S</span>
+        </a>
+        <a href="#" onclick="loadRoom('room4')">
+            <span class="text">Grupos</span>
+        </a>
+    </div>
+    <div class="content">
+        <div id="roomContent"></div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script src="/telegram_webserver/js/script.js"></script>
+</body>
+</html>
